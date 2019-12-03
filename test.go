4c0ceb47181f586925d835fb939b76111ebb4411
jake <jake@localhost.localdomain>
2019-12-04 03:33:58 +1100

    moved daemon to test

--- test.go
+++ test.go
@@ -5,12 +5,42 @@ import (
 	"io"
 	"log"
 	"github.com/gliderlabs/ssh"
+	"github.com/sevlyar/go-daemon"
 )
 
+// To terminate the daemon use:
+//  kill `cat sample.pid`
 func main() {
-	ssh.Handle(func(s ssh.Session) {
+	cntxt := &daemon.Context{
+		PidFileName: "sample.pid",
+		PidFilePerm: 0644,
+		LogFileName: "sample.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon sample]"},
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatal("Unable to run: ", err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	log.Print("- - - - - - - - - - - - - - -")
+	log.Print("daemon started")
+
+	serveSSH()
+}
+
+func serveSSH() {
+	ssh.Handle(func(s ssh.Session){
 		io.WriteString(s, fmt.Sprintf("Hello %s\n", s.User()))
 	})
-	log.Println("starting ssh server on port 2222...")
+	log.Println("Server on port 2222...")
 	log.Fatal(ssh.ListenAndServe(":2222", nil))
 }
+
