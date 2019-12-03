f003d83b71df12d377300bf805131557efa1172d
jake <jake@localhost.localdomain>
2019-12-04 02:42:38 +1100

    updated dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM scratch
-ADD main /
-CMD ["/main"]
+FROM alpine
+WORKDIR /usr/src/app
+EXPOSE 2222
+COPY main .
+CMD ["./main"]
 
--- test.go
+++ test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-
 	"github.com/gliderlabs/ssh"
 )
 
@@ -12,7 +11,6 @@ func main() {
 	ssh.Handle(func(s ssh.Session) {
 		io.WriteString(s, fmt.Sprintf("Hello %s\n", s.User()))
 	})
-
 	log.Println("starting ssh server on port 2222...")
 	log.Fatal(ssh.ListenAndServe(":2222", nil))
 	for{
